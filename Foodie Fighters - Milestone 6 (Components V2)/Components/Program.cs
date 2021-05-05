4eb68a4959c0c32fe3c6787751bca43ff99d1589
icequeen101 <43222443+icequeen101@users.noreply.github.com>
2021-05-04 23:50:58 -0700

    Milestone 6 - Component V2
    Noah, pls.

---
+++ Foodie Fighters - Milestone 6 (Components V2)/Components/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//We had an extension from Dr. Cai for extenuating circumstances so please accept this.
+
+
+
+namespace _322_Foodie_Fighters
+{
+
+    public class Waiter
+    {
+        private List<Order> currentOrders;
+        public void addOrder(Order o)
+        {
+
+        }
+        public void removeOrder(Order o)
+        {
+
+        }
+        private void processBill()
+        {
+
+        }
+        public void tellCookingTimerOrder(double d)
+        {
+
+        }
+    }
+    public class Order
+    {
+        private double priceOfDishes;
+        private List<Dish> dishes;
+        public double calculatePrice()
+        {
+            throw new NotImplementedException("Not yet implemented"); //TO-DO
+        }
+        public void addDish(Dish d)
+        {
+
+        }
+        public void removeDish(Dish d)
+        {
+
+        }
+        public void addSubstitution()
+        {
+
+        }
+    }
+    public class Dish
+    {
+        public string dishName;
+        private int itemsInMeal;
+        public double mealTimeToPrepare;
+        private double mealPrice;
+
+        public void substituteItem()
+        {
+
+        }
+    }
+    public class Item 
+    {
+        public string itemName;
+        private double itemPrice;
+        private int itemQuantity;
+        void updateQuantity(int n)
+        {
+
+        }
+    }
+
+
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ Foodie Fighters - Milestone 6 (Components V2)/Components/Trends_Stats_management.cs
@@ -0,0 +1,47 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _322_Foodie_Fighters
+{
+    static public class TrendsStatsManagement
+    {
+        static public class Inventory
+        {
+            static public Dictionary<Item, int> itemList;
+        }
+        static public class Database
+        {
+            static public Dictionary<Item, double> itemPrices;
+            static public Dictionary<Dish, double> menuPrices;
+            static public Dictionary<Dish, int> dishesOrdered;
+            static public Dictionary<Dish, double> prepTimeRecords;
+            static public void recordDishOrdered(Dish d, int quantity)
+            {
+
+            }
+        }
+        static public class TrendCalculator
+        {
+            static public void rankMostPopularItems()
+            {
+                //Should be a simple sort on dishesOrdered
+            }
+            static public void rankDishPrepTimes()
+            {
+                //Should be a simple sort on dishesOrdered
+            }
+            static public double findAvgPrepTime(Dish d)
+            {
+                //Should be a simple search in prepTimeRecords and divide by the value from dishesOrdered
+                int num = Database.dishesOrdered[d];
+                double totalPrepTime = Database.prepTimeRecords[d];
+                return totalPrepTime / num;
+            }
+        }
+
+
+    }
+}
